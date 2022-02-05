3194173ea90906a1ab1d511ed9b9d4b19d62369d
shawntvoltaire <shawn.t.voltaire@gmail.com>
2022-02-04 22:59:01 -0500

    about

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 
 const express = require('express')
+const path = require("path");
 const app = express()
 const port = 3000
 
 //public dir
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(path.join(__dirname + '/public')));
+app.use('/about', express.static(path.join(__dirname + '/public/index.html')));
 
 app.get('/', (req, res) => {
   res.send('index.html')
